0667a5bd935a7987140bbd7a3460094bbf47f75d
EC2 Default User <ec2-user@ip-172-31-21-124.ap-northeast-1.compute.internal>
2020-01-04 17:34:36 +0900

    edit logger

--- node_functions/utils/logger.py
+++ node_functions/utils/logger.py
@@ -1,9 +1,12 @@
 import datetime
 import watchtower, logging
+import boto3
+
+session = boto3.Session(region_name='ap-northeast-1')
 
 logging.basicConfig(level=logging.INFO)
 _logger = logging.getLogger('sample_logger')
-_logger.addHandler(watchtower.CloudWatchLogHandler())
+_logger.addHandler(watchtower.CloudWatchLogHandler(boto3_session=session))
 
 def info(message):
     _logger.info('[%s] %s' % (current_datetime(), message))
